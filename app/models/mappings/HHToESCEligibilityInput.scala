e268646364c13d7f1512e144414c4bd99612355d
David Wright <davew@MacBook-Pro.local>
2017-08-01 16:06:57 +0100

    mapping for houshold model to elibility input

---
+++ app/models/mappings/HHToESCEligibilityInput.scala
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.mappings
+
+import java.util.{Calendar, Date}
+
+import config.ApplicationConfig
+import models._
+import models.input.esc._
+import org.joda.time.LocalDate
+
+object HHToESCEligibilityInput {
+
+  def convert(hh:Household):ESCEligibilityInput = {
+    ESCEligibilityInput(escTaxYears = createTaxYears(hh))
+  }
+
+  private def createTaxYears(hh:Household):List[ESCTaxYear] = {
+    val now = ApplicationConfig.startDate
+    val april6thCurrentYear = determineApril6DateFromNow(now)
+    val claimantList = hhClaimantToESCEligibilityInputClaimant(hh.parent, hh.partner)
+    val childList = hhChildToESCEligibilityInputChild(hh.children)
+
+    List(
+      ESCTaxYear(
+        from = now,
+        until = april6thCurrentYear,
+        claimants = claimantList,
+        children = childList
+      ),
+      ESCTaxYear(
+        from = april6thCurrentYear,
+        until = now.plusYears(1),
+        claimants = claimantList,
+        children = childList
+      )
+    )
+  }
+
+  private def hhClaimantToESCEligibilityInputClaimant(hhParent: Claimant, hhPartner: Option[Claimant]): List[ESCClaimant] = {
+
+    val parent: ESCClaimant = ESCClaimant(isPartner = false, employerProvidesESC = convertVouchers(hhParent))
+
+    hhPartner match {
+      case Some(hhPartner) => List(parent, ESCClaimant(isPartner = true, employerProvidesESC = convertVouchers(hhParent)))
+      case None => List(parent)
+    }
+  }
+
+  private def hhChildToESCEligibilityInputChild(hhChildren: List[Child]): List[ESCChild] = {
+    hhChildren map (child => {
+      ESCChild(
+        id = 1,
+        dob = child.dob.get,
+        childCareCost = child.childcareCost match {
+          case Some(childcareCost) => childcareCost.amount.getOrElse(BigDecimal(0.00))
+          case None => BigDecimal(0.00)
+        }
+      ,
+        childCareCostPeriod = PeriodEnumToPeriod.convert(child.childcareCost match {
+          case Some(childcareCost) => childcareCost.period.get
+          case None => PeriodEnum.INVALID
+        }),
+        disability = ESCDisability(
+                                  disabled = child.disability.get.disabled || child.disability.get.blind,
+                                  severelyDisabled = child.disability.get.severelyDisabled)
+        )
+    })
+  }
+
+
+  private def determineApril6DateFromNow(from: LocalDate) : LocalDate = {
+    val currentCalendar = Calendar.getInstance()
+    currentCalendar.clear()
+    currentCalendar.setTime(from.toDate)
+    val periodYear = currentCalendar.get(Calendar.YEAR)
+    val periodStart = from.toDate
+    val january1st = getCalendarMonth(periodYear, Calendar.JANUARY, 1)
+    val april6CurrentYear = getCalendarMonth(periodYear, Calendar.APRIL, 6)
+
+    val aprilCalendarNextYear = Calendar.getInstance()
+    aprilCalendarNextYear.clear()
+    currentCalendar.setTime(april6CurrentYear)
+    aprilCalendarNextYear.set(Calendar.YEAR, periodYear + 1)
+    aprilCalendarNextYear.set(Calendar.MONTH, Calendar.APRIL)
+    aprilCalendarNextYear.set(Calendar.DAY_OF_MONTH, 6)
+    val april6NextYear = aprilCalendarNextYear.getTime
+
+    if ((periodStart.compareTo(january1st) == 0 || periodStart.after(january1st)) && periodStart.before(april6CurrentYear)) {
+      LocalDate.fromDateFields(april6CurrentYear)
+    } else {
+      LocalDate.fromDateFields(april6NextYear)
+    }
+  }
+
+  private def getCalendarMonth(periodYear: Int, calendarMonth: Int, dayOfMonth: Int): Date = {
+    val monthCalendar = Calendar.getInstance()
+    monthCalendar.clear()
+    monthCalendar.set(Calendar.YEAR, periodYear)
+    monthCalendar.set(Calendar.MONTH, calendarMonth)
+    monthCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+    monthCalendar.getTime
+  }
+
+  private def convertVouchers(hhClaimant: Claimant): Boolean = {
+    hhClaimant.escVouchers match {
+      case YesNoUnsureBothEnum.BOTH => true
+      case YesNoUnsureBothEnum.NO => false
+      case YesNoUnsureBothEnum.NOTSURE => true
+      case YesNoUnsureBothEnum.YES => true
+      case _ => true
+    }
+  }
+}
---
+++ test/mapping/HHToESCEligibilityInputSpec.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mapping
+
+import java.time.Month
+
+import controllers.FakeCCEligibilityApplication
+import models._
+import models.input.esc._
+import models.mappings.HHToESCEligibilityInput
+import org.joda.time.LocalDate
+import spec.CCConfigSpec
+import utils.Periods
+
+class HHToESCEligibilityInputSpec extends CCConfigSpec with FakeCCEligibilityApplication{
+
+val SUT = HHToESCEligibilityInput
+
+  "ESCInputEligibility" should {
+
+    "accept a valid Household model and return a valid ESCEligibilityInput model" in {
+
+      val hhChild1 = Child(
+        id = 0,
+        name = "child1",
+        dob = Some(LocalDate.parse("2016-08-31", formatter)),
+        disability = Some(Disability(disabled = true, severelyDisabled = false, blind = true)),
+        childcareCost = Some(ChildCareCost(amount = Some(350), Some(PeriodEnum.MONTHLY))),
+        education = Some(Education(inEducation = false, startDate = Some(LocalDate.now())))
+      )
+      val hhChild2 = Child(
+        id = 1,
+        name = "child2",
+        dob = Some(LocalDate.parse("2016-08-31", formatter)),
+        disability = Some(Disability(disabled = false, severelyDisabled = false, blind = true)),
+        childcareCost = Some(ChildCareCost(amount = Some(1000), Some(PeriodEnum.MONTHLY))),
+        education = Some(Education(inEducation = false, startDate = Some(LocalDate.now())))
+      )
+      val parent = Claimant(
+        ageRange = Some(AgeRangeEnum.EIGHTEENTOTWENTY),
+        benefits = Some(Benefits(
+          disabilityBenefits = false,
+          highRateDisabilityBenefits = false,
+          incomeBenefits = false,
+          carersAllowance = false
+        )),
+        lastYearlyIncome = None,
+        currentYearlyIncome = Some(Income(employmentIncome = Some(12212),
+          pension = Some(47674),
+          otherIncome = Some(647864),
+          benefits = Some(546),
+          statutoryIncome = None
+        )
+        ),
+        hours = Some(4567),
+        minimumEarnings = None,
+        escVouchers = Some(YesNoUnsureBothEnum.YES)
+      )
+      val partner = Claimant(
+        ageRange = Some(AgeRangeEnum.EIGHTEENTOTWENTY),
+        benefits = Some(Benefits(
+          disabilityBenefits = false,
+          highRateDisabilityBenefits = false,
+          incomeBenefits = false,
+          carersAllowance = false
+        )),
+        lastYearlyIncome = None,
+        currentYearlyIncome = Some(Income(employmentIncome = Some(12212),
+          pension = Some(47674),
+          otherIncome = Some(647864),
+          benefits = Some(546),
+          statutoryIncome = None
+        )
+        ),
+        hours = Some(4567),
+        minimumEarnings = None,
+        escVouchers = Some(YesNoUnsureBothEnum.NOTSURE)
+      )
+      val hhModel = Household(None, Some(LocationEnum.ENGLAND), true, List(hhChild1, hhChild2), parent, Some(partner))
+
+      val expectedOutput = ESCEligibilityInput(List(
+        ESCTaxYear(LocalDate.parse("2016-08-31", formatter), LocalDate.parse("2016-08-31", formatter),
+      List(ESCClaimant(false,true), ESCClaimant(true,true)),List(ESCChild(1,LocalDate.parse("2016-08-31", formatter),350,Periods.Monthly,ESCDisability(true,false)),
+          ESCChild(1,LocalDate.parse("2016-08-31", formatter),1000,Periods.Monthly,ESCDisability(true,false)))),
+        ESCTaxYear(LocalDate.parse("2016-08-31", formatter),LocalDate.parse("2016-08-31", formatter),
+      List(ESCClaimant(false,true), ESCClaimant(true,true)),List(ESCChild(1,LocalDate.parse("2016-08-31", formatter),350,Periods.Monthly,ESCDisability(true,false)),
+            ESCChild(1,LocalDate.parse("2016-08-31", formatter),1000,Periods.Monthly,ESCDisability(true,false))))))
+
+      val output = SUT.convert(hhModel)
+      println(output)
+      output.isInstanceOf[ESCEligibilityInput] shouldBe true
+      output shouldBe expectedOutput
+    }
+  }
+}
