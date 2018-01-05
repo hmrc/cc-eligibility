43ee3666c1b79cedf5654ea78988b2a7072d5682
shridhaarchippaa <shridhaar.chippaa@accenture.com>
2018-01-05 11:12:08 +0000

    Updated header

--- app/models/Household.scala
+++ app/models/Household.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -137,4 +137,3 @@ case class Household(
 object Household {
   implicit val formatHousehold = Json.format[Household]
 }
-
--- app/utils/SchemeConfig.scala
+++ app/utils/SchemeConfig.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -125,4 +125,3 @@ trait CCConfig extends LoadConfig {
 }
 
 object CCConfig extends CCConfig
-
