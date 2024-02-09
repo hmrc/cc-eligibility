f85fa790ea7c8582881c170f035455ff2b72e0f6
Josh Miernik <josh.miernik@digital.hmrc.gov.uk>
2024-02-09 13:50:33 +0000

    code

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,13 +7,15 @@ object AppDependencies {
   private val typesafe = "com.typesafe.play"
   private val hmrc = "uk.gov.hmrc"
 
-  private val bootstrapVersion = "7.15.0"
+  private val bootstrapVersion = "8.4.0"
+  private val pekkoVersion = "1.0.2"
+
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    hmrc                %% "bootstrap-backend-play-28" % bootstrapVersion,
+    hmrc                %% "bootstrap-backend-play-30" % bootstrapVersion,
     "com.github.fge"    % "json-schema-validator"      % "2.2.14",
-    typesafe            %% "play-json-joda"            % "2.9.4"
+    typesafe            %% "play-json-joda"            % "2.10.4"
   )
 
   trait TestDependencies {
@@ -24,11 +26,10 @@ object AppDependencies {
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test = Seq(
-        "uk.gov.hmrc"               %% "bootstrap-test-play-28"     % bootstrapVersion     % scope,
+        "uk.gov.hmrc"               %% "bootstrap-test-play-30"     % bootstrapVersion     % scope,
         "org.scalatestplus"         %% "scalatestplus-mockito"      % "1.0.0-M2"           % scope,
         "org.pegdown"               % "pegdown"                     % "1.6.0"              % scope,
-        "org.mockito"               % "mockito-core"                % "5.3.0"              % scope,
-        typesafe                    %% "play-test"                  % PlayVersion.current  % scope
+        "org.mockito"               % "mockito-core"                % "5.10.0"              % scope
       )
     }.test
   }
