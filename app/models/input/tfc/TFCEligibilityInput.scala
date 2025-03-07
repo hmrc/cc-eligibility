52a71cda1dc51a9855290472973f6a7a8d30c3d1
kwame-f-manso-acc <171707914+kwame-f-manso-acc@users.noreply.github.com>
2025-03-07 11:01:03 +0000

    updated getIncomeElement function

--- app/models/input/tfc/TFCEligibilityInput.scala
+++ app/models/input/tfc/TFCEligibilityInput.scala
@@ -90,7 +90,6 @@ object TFCIncome {
 }
 
 case class TFCClaimant(
-                        previousIncome: Option[TFCIncome] = None,
                         currentIncome: Option[TFCIncome] = None,
                         hoursPerWeek: Double = 0.00,
                         isPartner: Boolean = false,
@@ -103,7 +102,7 @@ case class TFCClaimant(
                         maximumEarnings: Option[Boolean] = None
                      ) {
   def totalIncome: BigDecimal = {
-    val (currentEmployment, currentvOther, currentPension) = getIncomeElements(previousIncome, currentIncome)
+    val (currentEmployment, currentvOther, currentPension) = getIncomeElements( currentIncome)
     getTotalTFCIncome(currentEmployment.getOrElse(ConfigConstants.defaultAmount),
       currentvOther.getOrElse(ConfigConstants.defaultAmount),
       currentPension.getOrElse(ConfigConstants.defaultAmount))
@@ -114,14 +113,13 @@ case class TFCClaimant(
     case _ => (None, None, None)
   }
 
-  private def getIncomeElements(previousIncome: Option[TFCIncome], currentIncome: Option[TFCIncome] ) = {
+  private def getIncomeElements(currentIncome: Option[TFCIncome] ) = {
 
-    val (empPrevious, otherPrevious, pensionPrevious) = determineIncomeElems(previousIncome)
     val (emp, other, pension) = determineIncomeElems(currentIncome)
 
-    (if(emp.isDefined) emp else empPrevious,
-    if(other.isDefined) other else otherPrevious,
-    if(pension.isDefined) pension else pensionPrevious)
+    (if(emp.isDefined) emp else None,
+    if(other.isDefined) other else None,
+    if(pension.isDefined) pension else None)
   }
 
   private def getTotalTFCIncome(employmentIncome: BigDecimal,
@@ -134,7 +132,6 @@ case class TFCClaimant(
 object TFCClaimant {
 
   implicit val claimantReads: Reads[TFCClaimant] = (
-    (JsPath \ "previousIncome").readNullable[TFCIncome] and
       (JsPath \ "currentIncome").readNullable[TFCIncome] and
         (JsPath \ "hoursPerWeek").read[Double].orElse(Reads.pure(0.00)) and
           (JsPath \ "isPartner").read[Boolean].orElse(Reads.pure(false)) and
